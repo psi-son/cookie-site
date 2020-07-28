9a97a81cc81de4785652d9688c9bd95c0c6d5d61
Triton Dev Wolfe <triton.wolfe@gmail.com>
2020-07-28 17:37:27 -0500

    Updates

---
+++ precache-manifest.16dc6438039a13aae1fa051de0e607cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920ba430927d619a0faedc41caab17dc",
+    "url": "/cookie-site/index.html"
+  },
+  {
+    "revision": "be3a557d04d1ac654e04",
+    "url": "/cookie-site/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "be3a557d04d1ac654e04",
+    "url": "/cookie-site/static/js/2.c40db26f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cookie-site/static/js/2.c40db26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd60dc0c7ad77029fd87",
+    "url": "/cookie-site/static/js/main.a6b0d7b7.chunk.js"
+  },
+  {
+    "revision": "41ba25d38c7b523461dc",
+    "url": "/cookie-site/static/js/runtime-main.f1b55c81.js"
+  }
+]);
